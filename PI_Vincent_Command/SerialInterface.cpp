26d803d4671b3b6c6773bf242330ac5e8fa9374a
Vincent <vs38190@gmail.com>
2017-04-03 20:59:01 +0200

    Travaille sur la classe télécomande

--- PI_Vincent_Command/SerialInterface.cpp
+++ PI_Vincent_Command/SerialInterface.cpp
@@ -5,3 +5,64 @@
 #include "SerialInterface.h"
 
 
+template<typename T>
+inline T SerialInterface::read<T>()
+{
+	T temp;
+	SerialInterface::read((pbyte)&temp, sizeof(T));
+	return temp;
+}
+template<typename T>
+inline void SerialInterface::write<T>(T const &var)
+{
+	SerialInterface::write((pbyte)&var, sizeof(var));
+	return;
+}
+void SerialInterface::read(pbyte buffer, uint len)
+{
+	m_interface.readBytes(buffer,len);
+	return;
+}
+void SerialInterface::write(const pbyte buffer, uint len)
+{
+	m_interface.write(buffer, len);
+	return;
+}
+
+inline void Telecommande::start()
+{
+	while (true)
+	{
+
+
+
+	}
+	return;
+}
+inline uint Telecommande::readX()
+{
+	uint var[TRIES];
+	FOR(i, TRIES)
+	{
+		var[i] = analogRead(PIN_X);
+	}
+	return Telecommande::average<TRIES>(var);
+}
+inline uint Telecommande::readY()
+{
+	uint var[TRIES];
+	FOR(i, TRIES)
+	{
+		var[i] = analogRead(PIN_Y);
+	}
+	return Telecommande::average<TRIES>(var);
+}
+inline uint Telecommande::readSpeed()
+{
+	uint var[TRIES];
+	FOR(i, TRIES)
+	{
+		var[i] = analogRead(PIN_SPEED);
+	}
+	return Telecommande::average<TRIES>(var);
+}
--- PI_Vincent_Command/SerialInterface.h
+++ PI_Vincent_Command/SerialInterface.h
@@ -1,5 +1,6 @@
 // SerialInterface.h
 
+#include <SoftwareSerial\src\SoftwareSerial.h>
 #ifndef _SERIALINTERFACE_h
 #define _SERIALINTERFACE_h
 
@@ -8,6 +9,83 @@
 #else
 	#include "WProgram.h"
 #endif
+
+#define TRIES 10
+#define FOR(var,min,max) for(uint var = min;var<max;++var)
+#define FOR(var,max) for(uint var = 0;var<max;++var)
+
+typedef unsigned int uint;
+typedef byte *pbyte;
+
+typedef struct TelecommandeToSystem
+{
+	byte x;
+	byte y;
+	byte speed;
+	int sum;
+}TDATA,*PTDATA;
+typedef struct SytemToTelecommande
+{
+	byte power;
+
+}SDATA,*PSDATA;
+
+
+
+class SerialInterface
+{
+public:
+	SerialInterface(const byte &RX, const byte &TX,const uint &bps = 9600) : PIN_TX(TX), PIN_RX(RX),m_interface(RX,TX)
+	{
+		m_interface.begin(bps);
+	}
+
+protected:
+	void read(pbyte buffer, uint len);
+	void write(const pbyte buffer, uint len);
+	template<typename T>
+	inline T read();
+	template<typename T>
+	inline void write(T const &var);
+
+	const byte PIN_TX;
+	const byte PIN_RX;
+	SoftwareSerial m_interface;
+};
+
+class Telecommande : SerialInterface
+{
+public:
+	Telecommande(const byte &RX, const byte &TX, const byte &POT_SPEED, const byte &POT_X, const byte &POT_Y, const uint &bps = 9600) : SerialInterface(RX, TX, bps), PIN_SPEED(POT_SPEED), PIN_X(POT_X), PIN_Y(POT_Y)
+	{}
+	inline void start();
+
+protected:
+	inline uint readX();
+	inline uint readY();
+	inline uint readSpeed();
+	
+	template<uint num>
+	average(uint* value)
+	{
+		uint rep;
+		FOR(i, num)
+		{
+			rep += value[i];
+		}
+		return rep / num;
+	}
+
+
+	const byte PIN_SPEED;
+	const byte PIN_X;
+	const byte PIN_Y;
+
+	TDATA tdata[2];
+	CDATA cdata[2];
+};
+
+
 
 
 #endif
