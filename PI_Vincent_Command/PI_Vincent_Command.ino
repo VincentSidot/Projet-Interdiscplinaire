6949b1e86156685b416364841c681f38524aa504
Vincent <vs38190@gmail.com>
2017-04-03 20:16:07 +0200

    Created the project

--- PI_Vincent_Command/PI_Vincent_Command.ino
+++ PI_Vincent_Command/PI_Vincent_Command.ino
@@ -1,13 +1,13 @@
 /*
- Name:		PI_Vincent_System.ino
- Created:	4/3/2017 8:13:19 PM
+ Name:		PI_Vincent_Command.ino
+ Created:	4/3/2017 8:15:09 PM
  Author:	Vincent
 */
 
 // the setup function runs once when you press reset or power the board
 #include "SerialInterface.h"
 void setup() {
-	Serial.begin(9600);
+
 }
 
 // the loop function runs over and over again until power down or reset
---
+++ PI_Vincent_System/SerialInterface.h
@@ -0,0 +1,14 @@
+// SerialInterface.h
+
+#ifndef _SERIALINTERFACE_h
+#define _SERIALINTERFACE_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
